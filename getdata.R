21c42754eb9adbcd9f1fa11be7b7c461d454f5d7
songxh0424 <songxh@umich.edu>
2017-11-13 15:21:27 -0500

    added check of SR in scraper

--- getdata.R
+++ getdata.R
@@ -2,12 +2,12 @@ source('./scraper.R')
 library(foreach)
 library(doParallel)
 
-## dfPlayer = getTags()
-## save(dfPlayer, file = 'dfPlayer.RData')
+cores = 26
+dfPlayer = getTags(cores = cores)
+save(dfPlayer, file = 'dfPlayer.RData')
 
 ## try out foreach in luigi
-load('./dfPlayer.RData')
-cores = 26
+## load('./dfPlayer.RData')
 registerDoParallel(cores = cores)
 dfDetail = foreach(b = dfPlayer$btag) %dopar%
   tryCatch(getTable(b), error = function(e) {print(sprintf('Player %s data unavailable!', b)); return(NULL)})
--- scraper.R
+++ scraper.R
@@ -5,6 +5,8 @@ library(data.table)
 library(rvest)
 library(parallel)
 library(stringr)
+library(foreach)
+library(doParallel)
 
 ## get a massive amount of battle tags
 genurl = function(page) {
@@ -12,7 +14,12 @@ genurl = function(page) {
   return(url)
 }
 
-getTags = function(pages = 1:1650, random = TRUE, n.per.page = 10, thres = 50) {
+## ## determine which server is the player on, based on SR
+## whichServer = function(btag, sr) {
+
+## }
+
+getTags = function(pages = 1:1650, n.per.page = 10, thres = 50, cores = 2) {
   tags = lapply(pages, function(p) {
     url = genurl(p)
     nodes = p %>% genurl() %>% read_html()
@@ -26,19 +33,34 @@ getTags = function(pages = 1:1650, random = TRUE, n.per.page = 10, thres = 50) {
     ## keep only rows that have at least 50 games
     games = rows %>% html_nodes('.align-right') %>% html_text() %>% str_trim() %>% as.numeric()
     idx = which(games >= thres)
-    if(random) {
-      idx = sample(idx, min(n.per.page, length(idx)))
-      rows = rows[idx]
-    }
     ## get the battle tags
     tag = rows %>% html_nodes('a') %>% html_text()
-    tag = tag[tag != '']
+    tag = tag[str_trim(tag) != '']
     tag = str_replace_all(tag, "#", "-")
     ## get the ranking and SR and games
     ranking = rows %>% html_node('td') %>% html_text() %>% as.numeric()
-    sr = rows %>% html_nodes('span') %>% html_text() %>% as.numeric()
-    games = games[idx]
-    return(data.frame(btag = tag, ranking = ranking, sr = sr, games = games))
+    sr = rows %>% html_node('span') %>% html_text() %>% as.numeric()
+    ## keep only rows that have the same SR as the one on US server
+    ## registerDoParallel(cores = cores)
+    ## idx.match = foreach(i = idx) %dopar%
+    ##   tryCatch({
+    ##     url_us = "https://playoverwatch.com/en-gb/career/pc/us/" %>% paste0(tag[i])
+    ##     webHTML = read_html(url_us)
+    ##     SR = webHTML %>% html_node('.u-align-center') %>% html_text() %>% as.numeric()
+    ##     SR == sr[i]
+    ##   }, error = function(e) return(FALSE))
+    idx.match = lapply(idx, function(i) { 
+      tryCatch({
+        url_us = "https://playoverwatch.com/en-gb/career/pc/us/" %>% paste0(tag[i])
+        webHTML = read_html(url_us)
+        SR = webHTML %>% html_node('.u-align-center') %>% html_text() %>% as.numeric()
+        SR == sr[i]
+      }, error = function(e) return(FALSE))
+    })
+    ## }, mc.cores = cores)
+    idx = idx[which(idx.match %>% unlist())]
+    idx = sample(idx, min(length(idx), n.per.page))
+    return(data.frame(btag = tag, ranking = ranking, sr = sr, games = games)[idx,])
   }) %>% bind_rows()
   return(tags)
 }
@@ -57,21 +79,22 @@ getTable = function(btag) {
   # table sections
   tabSection = innerNodes %>% html_nodes("div[data-group-id='stats']")
   # Loop over all tables
-  tables = list()
-  for (whichTable in 1:length(tabSection)) {
-    # Get table id
+  tables = lapply(1:length(tabSection), function(whichTable) {
+    ## Get table id
     tabID = tabSection[whichTable] %>% html_attr("data-category-id")
     tabSel = paste0("div[data-category-id=", "'", tabID, "'", "]") # select the given table
-    # Get table information
+                                        # Get table information
     dataTables = innerNodes %>% html_nodes(tabSel) %>% html_nodes("table.data-table")
     allTables = dataTables %>% html_table
     ## Now, up to you how you want to store the data... Will just store as vector of data frames
     whichHero = heroName[grep(tabID, idAttr)]
-    # just going to set the names of each data frame in the list
+                                        # just going to set the names of each data frame in the list
     names(allTables) = rep(paste0(btag, "-", whichHero, "-",as.character(unlist(lapply(allTables, function(x) { names(x)[1]})))), 1, length(allTables))
-    # store in proper one
-    tables = c(tables, allTables)
-  }
+                                        # store in proper one
+    ## tables = c(tables, allTables)
+    return(allTables)
+  })
+  tables = unlist(tables, recursive = FALSE)
   names(tables) = str_replace_all(names(tables), paste0(btag, '-'), '')
   return(tables)
 }
