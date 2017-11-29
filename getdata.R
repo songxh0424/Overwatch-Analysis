c9c6028906279b1d29ee08fd77f342a9d2a19630
songxh0424 <songxh@umich.edu>
2017-11-29 02:08:09 -0500

    updated methods to get data

--- getdata.R
+++ getdata.R
@@ -2,13 +2,23 @@ source('./scraper.R')
 library(foreach)
 library(doParallel)
 
-cores = 26
-dfPlayer = getTags(cores = cores)
-save(dfPlayer, file = 'dfPlayer.RData')
+cores = 24
+## check if RData exists, if not, run getTags to get btags
+if(file.exists('dfPlayer.RData')) {
+  load('./dfPlayer.RData')
+} else {
+  btags = getTags(n.per.page = 10, cores = cores)
+  save(btags, file = 'dfPlayer.RData')
+}
 
-## try out foreach in luigi
-## load('./dfPlayer.RData')
-registerDoParallel(cores = cores)
-dfDetail = foreach(b = dfPlayer$btag) %dopar%
-  tryCatch(getTable(b), error = function(e) {print(sprintf('Player %s data unavailable!', b)); return(NULL)})
-save(dfDetail, file = 'dfDetail.RData')
+## check if RData exists, if not, run getTable to get tables and SR 
+## try out foreach in mario 
+if(!file.exists('dfDetail.RData')) {
+  registerDoParallel(cores = cores)
+  dfDetail = foreach(btag = btags) %dopar%
+    tryCatch(getTable(btag),
+            error = function(e) {print(sprintf('Player %s data unavailable!', btag)); return(NULL)})
+            ## finally = {print(sprintf('player %s finished', i))})
+  names(dfDetail) = btags 
+  save(dfDetail, file = 'dfDetail.RData')
+}
--- scraper.R
+++ scraper.R
@@ -14,60 +14,46 @@ genurl = function(page) {
   return(url)
 }
 
-## ## determine which server is the player on, based on SR
-## whichServer = function(btag, sr) {
 
-## }
-
-getTags = function(pages = 1:1650, n.per.page = 10, thres = 50, cores = 2) {
-  tags = lapply(pages, function(p) {
-    url = genurl(p)
-    nodes = p %>% genurl() %>% read_html()
-    rows = nodes %>% html_nodes('tbody') %>% html_nodes('tr')
-    ## remove the ad row
-    if(length(rows) == 101) {
-      rows = rows[-35]
-    } else {
-      warning(sprintf('Page %s has wrong number of rows', p))
-    }
-    ## keep only rows that have at least 50 games
-    games = rows %>% html_nodes('.align-right') %>% html_text() %>% str_trim() %>% as.numeric()
-    idx = which(games >= thres)
-    ## get the battle tags
-    tag = rows %>% html_nodes('a') %>% html_text()
-    tag = tag[str_trim(tag) != '']
-    tag = str_replace_all(tag, "#", "-")
-    ## get the ranking and SR and games
-    ranking = rows %>% html_node('td') %>% html_text() %>% as.numeric()
-    sr = rows %>% html_node('span') %>% html_text() %>% as.numeric()
-    ## keep only rows that have the same SR as the one on US server
-    ## registerDoParallel(cores = cores)
-    ## idx.match = foreach(i = idx) %dopar%
-    ##   tryCatch({
-    ##     url_us = "https://playoverwatch.com/en-gb/career/pc/us/" %>% paste0(tag[i])
-    ##     webHTML = read_html(url_us)
-    ##     SR = webHTML %>% html_node('.u-align-center') %>% html_text() %>% as.numeric()
-    ##     SR == sr[i]
-    ##   }, error = function(e) return(FALSE))
-    idx.match = lapply(idx, function(i) { 
-      tryCatch({
-        url_us = "https://playoverwatch.com/en-gb/career/pc/us/" %>% paste0(tag[i])
-        webHTML = read_html(url_us)
-        SR = webHTML %>% html_node('.u-align-center') %>% html_text() %>% as.numeric()
-        SR == sr[i]
-      }, error = function(e) return(FALSE))
-    })
-    ## }, mc.cores = cores)
-    idx = idx[which(idx.match %>% unlist())]
-    idx = sample(idx, min(length(idx), n.per.page))
-    return(data.frame(btag = tag, ranking = ranking, sr = sr, games = games)[idx,])
-  }) %>% bind_rows()
+## get a bunch of random battle tags, ignore rankings and sr
+## the data on overwatchtracker can't be trusted
+getTags = function(pages = 1:1650, random = TRUE, n.per.page = 10, cores = 24) {
+  registerDoParallel(cores = cores)
+  ## tags = lapply(pages, function(p) {
+  tags = foreach(p = pages) %dopar%
+    tryCatch({
+      url = genurl(p)
+      nodes = p %>% genurl() %>% read_html()
+      rows = nodes %>% html_nodes('tbody') %>% html_nodes('tr')
+      ## remove the ad row
+      if(length(rows) == 101) {
+        rows = rows[-35]
+      } else {
+        warning(sprintf('Page %s has wrong number of rows', p))
+      }
+      ## get the battle tags
+      tag = rows %>% html_nodes('a') %>% html_text()
+      tag = tag[tag != '']
+      tag = str_replace_all(tag, "#", "-")
+      idx = 1:100
+      if(random) {
+        idx = sample(idx, min(n.per.page, length(idx)))
+      }
+      return(tag[idx])
+    }, error = function(e) {print(sprintf('Page %s has error', p)); return(NULL)})
+  ## } %>% unlist()
+  tags = unlist(tags)
   return(tags)
 }
-## get tables based on battle tag
+
 getTable = function(btag) {
   url_profile = "https://playoverwatch.com/en-gb/career/pc/us/" %>% paste0(btag)
-  webHTML = tryCatch({read_html(url_profile)}, error=function(err) "Error")
+  webHTML = read_html(url_profile)
+  # get sr
+  SR = webHTML %>% html_node('.u-align-center') %>% html_text() %>% as.numeric()
+  ## get main
+  main = webHTML %>% html_node('style') %>% html_text() %>% str_extract_all('hero/\\w+/career')
+  main = main[[1]][2] %>% str_replace('hero/', '') %>% str_replace('/career', '')
   # Get playmode
   innerNodes = webHTML %>% html_nodes("#competitive")
   
@@ -96,5 +82,5 @@ getTable = function(btag) {
   })
   tables = unlist(tables, recursive = FALSE)
   names(tables) = str_replace_all(names(tables), paste0(btag, '-'), '')
-  return(tables)
+  return(list(SR = SR, main = main, tables = tables))
 }
